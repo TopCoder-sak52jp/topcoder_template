b1de204b4c6a06a7107837efb0b621e24b19d482
Yuzo Saito <yuzo.saito387@gmail.com>
2016-03-02 13:43:11 -0500

    modify unit tests

--- topcoder_template_test/topcoder_template_test/ML_Tests.cs
+++ topcoder_template_test/topcoder_template_test/ML_Tests.cs
@@ -10,7 +10,7 @@ namespace topcoder_template_test
     [TestClass]
     public class ML_Tests
     {
-        const double EPS = 0.0001;
+        const double EPS = 0.01;
 
         [TestMethod]
         public void LinearRegressionTest()
@@ -18,31 +18,31 @@ namespace topcoder_template_test
             //y = 0.0 + 2.0 x1
             var x = new Matrix(new double[,] { { 1, 1 }, { 1, 2 }, { 1, 4 } });
             var y = new Matrix(new double[,] { { 2 }, { 4 }, { 8 } });
-            var ret = LinearRegression.GetResult(x, y, false, 0.01);
+            var ret = LinearRegression.GetResult_GradientDescent(x, y, 0.01, 0, 4000, false).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] - 0.0) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 2.0) < EPS);
-            ret = LinearRegression.GetResult(x, y);
+            ret = LinearRegression.GetResult_NormalEquation(x, y, 0).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] - 0.0) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 2.0) < EPS);
 
             //y = 1.5 + 3.5 x1
             x = new Matrix(new double[,] { { 1, 1 }, { 1, 2 }, { 1, 4 } });
             y = new Matrix(new double[,] { { 5 }, { 8.5 }, { 15.5 } });
-            ret = LinearRegression.GetResult(x, y, false, 0.01);
+            ret = LinearRegression.GetResult_GradientDescent(x, y, 0.01, 0, 4000, false).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] - 1.5) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 3.5) < EPS);
-            ret = LinearRegression.GetResult(x, y);
+            ret = LinearRegression.GetResult_NormalEquation(x, y, 0).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] - 1.5) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 3.5) < EPS);
 
             //y = -3.3 + 2.1 x1 - 0.03 x2
             x = new Matrix(new double[,] { { 1, 1, 2 }, { 1, 2, 2 }, { 1, 4, 5 } });
             y = new Matrix(new double[,] { { -1.26 }, { 0.84 }, { 4.95 } });
-            ret = LinearRegression.GetResult(x, y, false, 0.01);
+            ret = LinearRegression.GetResult_GradientDescent(x, y, 0.01, 0, 4000, false).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] + 3.3) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 2.1) < EPS);
             Assert.IsTrue(Math.Abs(ret[2, 0] + 0.03) < EPS);
-            ret = LinearRegression.GetResult(x, y);
+            ret = LinearRegression.GetResult_NormalEquation(x, y, 0).Item1;
             Assert.IsTrue(Math.Abs(ret[0, 0] + 3.3) < EPS);
             Assert.IsTrue(Math.Abs(ret[1, 0] - 2.1) < EPS);
             Assert.IsTrue(Math.Abs(ret[2, 0] + 0.03) < EPS);
